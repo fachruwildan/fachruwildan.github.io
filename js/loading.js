c458c642af69ef3b71519cee9a8a66df5b56cbdd
Fachru Wildan <fachruwildan1@gmail.com>
2022-10-04 21:09:23 +0700

    update : loading

---
+++ js/loading.js
@@ -0,0 +1,33 @@
+
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+/*This is main JS file that contains custom style rules used in this template*/
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+/* Template Name: "IVY"*/
+/* Version: 1.0 Initial Release*/
+/* Build Date: xx-xx-2016*/
+/* Author: UnionAgency*/
+/* Copyright: (C) 2016 */
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------*/
+/* TABLE OF CONTENTS: */
+/*--------------------------------------------------------*/
+/* 01 - VARIABLES */
+/* 02 - page calculations */
+/* 03 - function on document ready */
+/* 04 - function on page load */
+/* 05 - function on page resize */
+/* 06 - function on page scroll */
+/* 07 - swiper sliders */
+/* 08 - buttons, clicks, hovers */
+
+var _functions = {};
+
+$(function() {
+
+
+	$(window).on("load", function() {
+        $("#loading").fadeOut("slow");
+    });
+
+});
